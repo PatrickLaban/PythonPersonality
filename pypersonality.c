8a3fb5710bcc166f8d523fa6324140f391d663b7
Patrick Laban <laban.patrick@gmail.com>
2016-08-31 16:13:38 -0700

    set personality proto added

--- pypersonality.c
+++ pypersonality.c
@@ -1,9 +1,8 @@
 #include <Python.h>
 #include <sys/personality.h>
-#include <stdint.h>
 
-int32_t Cget_personality(void) {
-	unsigned long persona = 0xffffffff;
+int Cget_personality(void) {
+	unsigned long int persona = 0xffffffff;
 	return personality(persona);
 }
 
@@ -11,8 +10,18 @@ static PyObject* get_personality(PyObject* self) {
 	return Py_BuildValue("i", Cget_personality);
 }
 
+int Cset_personality(void) {
+	unsigned long int persona = READ_IMPLIES_EXEC;
+	return personality(persona);
+}
+
+static PyObject* set_personality(PyObject* self) {
+	return Py_BuildValue("i", Cset_personality);
+}
+
 static PyMethodDef personality_methods[] = {
 	{"get_personality", (PyCFunction)get_personality, METH_NOARGS, "Returns the personality value"},
+	{"set_personality", (PyCFunction)set_personality, METH_NOARGS, "Sets the personality value"},
 	/*	{"version", (PyCFunction)version, METH_NOARGS, "Returns the version number"}, */
 	{NULL, NULL, 0, NULL}
 };
